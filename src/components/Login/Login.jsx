6d576dc3fd0a8e0f3e3f095f8b004ea7b219111b
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2025-02-25 11:22:14 +0530

    25-02

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "../Login/Login.css";
 
 function Login() {
-  return <div></div>;
+  return (
+    <>
+      <div className="form-div">
+        <h1 className="mb-4">Welcome</h1>
+        <form>
+          <div className="form-group">
+            <label for="username">Enter Your Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div className="form-group">
+            <label for="password">Enter Your Password</label>
+            <input type="password" id="password" />
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Login;
